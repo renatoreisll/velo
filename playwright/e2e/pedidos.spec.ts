7a1b7fa5085974564c05e0c60f6f9c74fee8e329
renatociandt <79221803+renatociandt@users.noreply.github.com>
2026-05-27 11:13:44 -0400

    Retirada do ;.

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,29 +1,29 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('Deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('http://localhost:5173/')
 
   // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   // Checkpoint 2: Clicar no link de consulta de pedidos
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
 
   // Checkpoint 3: Verificar se a página de consulta de pedidos está carregada
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Checkpoint 4: Preencher o campo de busca de pedido
-  await page.getByTestId('search-order-id').fill('VLO-RBAQSL');
+  await page.getByTestId('search-order-id').fill('VLO-RBAQSL')
 
   //Checkpoint 5: Clicar no botão de busca de pedido
-  await page.getByTestId('search-order-button').click();
+  await page.getByTestId('search-order-button').click()
 
   //Checkpoint 6: Verificar se o pedido foi encontrado
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-RBAQSL');
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-RBAQSL')
 
   //Checkpoint 7: Verificar se o status do pedido é APROVADO  
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 
-});
+}) 
