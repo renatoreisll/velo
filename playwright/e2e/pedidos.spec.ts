e6fef863b0e72db3772e50f5d7327ec779465d12
renatociandt <79221803+renatociandt@users.noreply.github.com>
2026-06-03 13:46:12 -0400

    Adição dos hooks (ganchos).

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,61 +3,59 @@ import { generateOrderCode } from '../support/helpers'
 
 /// AAA - Arrange, Act, Assert
 
-test('Deve consultar um pedido aprovado', async ({ page }) => {
+test.describe('Consulta de pedido', () => {
 
-   // Test Data
+   test.beforeEach(async ({ page }) => {
+      // -------------------->  Arrange
+      // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
+      await page.goto('http://localhost:5173/')
+      await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
-   const order = 'VLO-RBAQSL'
+      // Checkpoint 2: Clicar no link de consulta de pedidos
+      await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+
+      // Checkpoint 3: Verificar se a página de consulta de pedidos está carregada
+      await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+   })
+
+   test('Deve consultar um pedido aprovado', async ({ page }) => {
+
+      // Test Data
+      const order = 'VLO-RBAQSL'
+
+      // -------------------->  Act
+      //Checkpoint 4: Preencher o campo de busca de pedido
+      await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+
+      //Checkpoint 5: Clicar no botão de busca de pedido
+      await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+      // -------------------->  Assert
+      //Checkpoint 6: Verificar se o pedido foi encontrado
+      const containerPedido = page.getByRole('paragraph')
+         .filter({ hasText: /^Pedido$/ })
+         .locator('..') //Sobe pro elemento pai
+
+      await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+
+      //Checkpoint 7: Verificar se o status do pedido é APROVADO  
+      await expect(page.getByText('APROVADO')).toBeVisible()
+
+   })
 
 
-   // -------------------->  Arrange
-   // Checkpoint 1: Verificar se a página de consulta de pedidos está carregada
-   await page.goto('http://localhost:5173/')
-   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+   test('Deve exibir mensagem quando o pedido não for encontrado', async ({ page }) => {
+      const order = 'generateOrderCode'
 
-   // Checkpoint 2: Clicar no link de consulta de pedidos
-   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+      await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
 
-   // Checkpoint 3: Verificar se a página de consulta de pedidos está carregada
-   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-   // -------------------->  Act
-   //Checkpoint 4: Preencher o campo de busca de pedido
-   await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
-
-   //Checkpoint 5: Clicar no botão de busca de pedido
-   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-
-   // -------------------->  Assert
-   //Checkpoint 6: Verificar se o pedido foi encontrado
-   const containerPedido = page.getByRole('paragraph')
-      .filter({ hasText: /^Pedido$/ })
-      .locator('..') //Sobe pro elemento pai
-
-   await expect(containerPedido).toContainText(order, { timeout: 10_000 })
-
-   //Checkpoint 7: Verificar se o status do pedido é APROVADO  
-   await expect(page.getByText('APROVADO')).toBeVisible()
+      await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+      await expect(page.locator('#root')).toMatchAriaSnapshot(`
+         - img
+         - heading "Pedido não encontrado" [level=3]
+         - paragraph: Verifique o número do pedido e tente novamente
+         `)
+   })
 
 })
 
-
-test('Deve exibir mensagem quando o pedido não for encontrado', async ({ page }) => {
-   const order = 'generateOrderCode'
-
-   await page.goto('http://localhost:5173/')
-   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-
-   await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
-
-   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-   await expect(page.locator('#root')).toMatchAriaSnapshot(`
-      - img
-      - heading "Pedido não encontrado" [level=3]
-      - paragraph: Verifique o número do pedido e tente novamente
-      `)
-})
